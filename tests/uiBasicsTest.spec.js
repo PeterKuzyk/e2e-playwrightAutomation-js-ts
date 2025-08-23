98ced6d2d3d197db04efde0e2d3f79cd582e8851
PeterKuzyk <55903558+PeterKuzyk@users.noreply.github.com>
2025-08-23 18:24:35 -0400

    added assertion demo

--- tests/uiBasicsTest.spec.js
+++ tests/uiBasicsTest.spec.js
@@ -15,3 +15,32 @@ test('Browser Context Declaration', async ({page}) => {
     expect(title).toBe('Google');
     await expect(page).toHaveTitle('Google');
 });
+
+test('Assertion demo failure', async ({browser}) => {
+const context = await browser.newContext();
+const page = await context.newPage();
+await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+await page.locator('#username').fill('rahulshettyacademy1');
+await page.locator('#password').fill('learning');
+await page.locator('#signInBtn').click();
+console.log( await page.locator("[style*='block']").textContent());
+await expect(page.locator("[style*='block']")).toContainText('Incorrect');
+});
+
+test.only('Assertion demo success', async ({browser}) => {
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    const userName = page.locator('#username');
+    const password = page.locator('#password');
+    const signInBtn = page.locator('#signInBtn');
+    await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+    await userName.fill('rahulshettyacademy');
+    await password.fill('learning');
+    await signInBtn.click();
+    console.log( await page.locator(".card-body a").first().textContent());
+    await expect(page.locator(".card-body a").first()).toHaveText('iphone X');
+    await expect(page.locator(".card-body a").nth(1)).toHaveText('Samsung Note 8');
+
+
+})
+
