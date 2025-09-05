6b749352f3927b9a4971d5168aa2fecad3cfdac4
PeterKuzyk <55903558+PeterKuzyk@users.noreply.github.com>
2025-09-05 16:02:25 -0400

    added new test cases for assertions

--- tests/uiBasicsTest.spec.js
+++ tests/uiBasicsTest.spec.js
@@ -17,17 +17,17 @@ test('Browser Context Declaration', async ({page}) => {
 });
 
 test('Assertion demo failure', async ({browser}) => {
-const context = await browser.newContext();
-const page = await context.newPage();
-await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
-await page.locator('#username').fill('rahulshettyacademy1');
-await page.locator('#password').fill('learning');
-await page.locator('#signInBtn').click();
-console.log( await page.locator("[style*='block']").textContent());
-await expect(page.locator("[style*='block']")).toContainText('Incorrect');
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+    await page.locator('#username').fill('rahulshettyacademy1');
+    await page.locator('#password').fill('learning');
+    await page.locator('#signInBtn').click();
+    console.log(await page.locator("[style*='block']").textContent());
+    await expect(page.locator("[style*='block']")).toContainText('Incorrect');
 });
 
-test.only('Assertion demo success', async ({browser}) => {
+test('Assertion demo success', async ({browser}) => {
     const context = await browser.newContext();
     const page = await context.newPage();
     const userName = page.locator('#username');
@@ -37,10 +37,24 @@ test.only('Assertion demo success', async ({browser}) => {
     await userName.fill('rahulshettyacademy');
     await password.fill('learning');
     await signInBtn.click();
-    console.log( await page.locator(".card-body a").first().textContent());
+    console.log(await page.locator(".card-body a").first().textContent());
     await expect(page.locator(".card-body a").first()).toHaveText('iphone X');
     await expect(page.locator(".card-body a").nth(1)).toHaveText('Samsung Note 8');
-
-
 })
 
+test('Assertion demo working with titles', async ({browser}) => {
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    const userName = page.locator('#username');
+    const password = page.locator('#password');
+    const signInBtn = page.locator('#signInBtn');
+    const cardTitles = page.locator(".card-body a");
+    await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+    await userName.fill('rahulshettyacademy');
+    await password.fill('learning');
+    await signInBtn.click();
+     console.log(await cardTitles.first().textContent());
+    // console.log(await cardTitles.nth(1).textContent());
+    const allTitles = await cardTitles.allTextContents();
+    console.log(allTitles);
+})
