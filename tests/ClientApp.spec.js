7c44428abc721dd7961a2251beaa682499967d6a
PeterKuzyk <55903558+PeterKuzyk@users.noreply.github.com>
2025-09-05 16:56:20 -0400

    added clientApp tests

---
+++ tests/ClientApp.spec.js
@@ -0,0 +1,19 @@
+const {test, expect} = require('@playwright/test');
+
+test.only('Practice Website', async ({page}) => {
+    const userEmail = page.locator(" #userEmail");
+    const password = page.locator(' #userPassword');
+    const login = page.locator(" #login");
+    const allCards = page.locator(".card-body b");
+
+    await page.goto('https://rahulshettyacademy.com/client/#/auth/login');
+    await userEmail.fill("toem@example.com");
+    await password.fill("n#4gRLp3w#Aq!9X");
+    await login.click();
+    // wait example
+    await page.waitForLoadState("networkidle");
+    // in case previous wait not working
+    await page.locator(".card-body b").first().waitFor();
+    await expect(allCards.first()).toHaveText("ZARA COAT 3");
+    console.log(await allCards.allTextContents());
+})
